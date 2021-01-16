f37d2a1e0ca603b62211588fd2071eb91b33cda4
athena255 <afc65@cornell.edu>
2021-01-15 22:12:42 -0500

    Test that works when block_len is the size of the cacheline

--- tests/test_specv1.cpp
+++ tests/test_specv1.cpp
@@ -11,7 +11,7 @@ unsigned int array1_size = 16;
 uint8_t unused[64]; // seperate by a cacheline
 uint8_t array1[160] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 uint8_t unused2[64]; // seperate by a cacheline
-uint8_t array2[MAX_BYTE*256];
+uint8_t array2[MAX_BYTE*64];
 // Whether secret is const char* or just char* matters
 const char* secret = "The cake is a lie!";
 
@@ -21,7 +21,7 @@ volatile uint8_t get_elem(size_t x)
     volatile uint8_t res;
     if (x < array1_size )
     {
-        res = array2[array1[x]*256];
+        res = array2[array1[x]*64];
     }
     return res;
 }
@@ -32,17 +32,16 @@ TEST_CASE("Spectrev1", "[branch]")
                 array2,
                 [](volatile size_t x){flush(&array1_size); get_elem(x);},
                 [](size_t _t){return _t%array1_size;},
-                256,
+                64,
                 MAX_TRIES,
                 20
     );
 
-    auto slen = strlen(secret);
     uint8_t val;
-    for (size_t i = 0; i  < slen; ++i)
+    for (size_t i = 0; i < 40; ++i)
     {
         s.read_byte(CVMEM_ADD(const_cast<char*>(secret), i), &val);
-        printf("0x%02X = %c\n", val, val);
+//        printf("0x%02X = %c\n", val, val);
         REQUIRE(val == secret[i]);
     }
 
