b16715703819bae6bc882352e43665dcbbeebb91
athena255 <afc65@cornell.edu>
2021-01-17 19:11:21 -0500

    modification to test_specv1

--- tests/test_specv1.cpp
+++ tests/test_specv1.cpp
@@ -11,9 +11,8 @@ unsigned int array1_size = 16;
 uint8_t unused[64]; // seperate by a cacheline
 uint8_t array1[160] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 uint8_t unused2[64]; // seperate by a cacheline
-uint8_t array2[MAX_BYTE*64];
-// If the bytes of secret are less than 16, then we will have trouble reading it
-// because these are legitimate values of array1
+uint8_t *array2;
+
 uint8_t secret[25] = {0xde, 0xad, 0xbe, 0xef, 0x69};
 
 volatile uint8_t get_elem(size_t x)
@@ -29,21 +28,24 @@ volatile uint8_t get_elem(size_t x)
 
 TEST_CASE("Spectrev1", "[branch]")
 {
+    array2 = (uint8_t*)malloc(256*BLOCK_LEN);
     Spectrev1 s(array1,
                 array2,
                 [](volatile size_t x){flush(&array1_size); get_elem(x);},
-                [](size_t _t){return _t%array1_size;},
+                [](size_t _t){return _t%16;},
                 64,
-                MAX_TRIES/8,
+                MAX_TRIES/4,
                 20
     );
 
     uint8_t val;
-    for (size_t i = 0; i < 5; ++i)
+    for (size_t i = 0; i < 25; ++i)
     {
         s.read_byte(&secret[i], &val);
-        REQUIRE(val == secret[i]);
         printf("0x%02X\n", val);
+        REQUIRE(val == secret[i]);
     }
+
+    free(array2);
 
 }
